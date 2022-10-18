335ccb3132e092ed334099054696dc907236ed62
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-18 16:56:54 +0100

    add packeges

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-
+import 'antd/dist/antd.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
+    <div className="App m-5">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Login" element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
