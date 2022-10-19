75bf4579eb419d0764c51f58b212d484924b60b1
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-20 00:10:06 +0100

    add PageTitle

---
+++ client/src/components/PageTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PageTitle({ title }) {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold">{title}</h1>
+    </div>
+  )
+}
+
+export default PageTitle
