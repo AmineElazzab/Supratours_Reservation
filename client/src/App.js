5f8a387f965ed388be0478932e44e6b3423fec54
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-20 00:14:08 +0100

    update App

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,24 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import PublicRoute from './components/PublicRoute';
 import ProtectedRoute from './components/ProtectedRoute';
+import AdminHome from './pages/Admin/AdminHome';
+import AdminBuses from './pages/Admin/AdminBuses';
+import AdminUsers from './pages/Admin/AdminUsers';
+import AdminBookings from './pages/Admin/AdminBookings';
+// import Loader from './components/Loader';
+
 
 function App() {
   return (
     <div className="App m-5">
+      {/* <Loader /> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<ProtectedRoute> <Home /> </ProtectedRoute>} />
+          <Route path="/admin" element={<ProtectedRoute> <AdminHome /> </ProtectedRoute>} />
+          <Route path="/admin/buses" element={<ProtectedRoute> <AdminBuses /> </ProtectedRoute>} />
+          <Route path="/admin/users" element={<ProtectedRoute> <AdminUsers /> </ProtectedRoute>} />
+          <Route path="/admin/bookings" element={<ProtectedRoute> <AdminBookings /> </ProtectedRoute>} />
           <Route path="/Register" element={<PublicRoute> <Register /> </PublicRoute>} />
           <Route path="/Login" element={<PublicRoute> <Login /> </PublicRoute>} />
         </Routes>
