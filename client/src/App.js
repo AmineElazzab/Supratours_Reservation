f2bd9971e5a1ffd83311c934dd6604829bfcc705
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-19 15:44:44 +0100

    update App.js & index.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <div className="App m-5">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/Login" element={<Login/>} />
+          <Route path="/" element={<ProtectedRoute> <Home /> </ProtectedRoute>} />
+          <Route path="/Register" element={<PublicRoute> <Register /> </PublicRoute>} />
+          <Route path="/Login" element={<PublicRoute> <Login /> </PublicRoute>} />
         </Routes>
       </BrowserRouter>
     </div>
