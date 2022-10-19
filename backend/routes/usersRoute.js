4224d61d1209adcd482a751424dd23735279db84
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-19 09:45:45 +0100

    update userRoute

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/usersModel');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
 //register new user
@@ -33,6 +34,42 @@ router.post('/register', async (req, res) => {
         })
     }
 
+});
+
+//login user
+router.post('/login', async (req, res) => {
+    try {
+        const userExists = await User.findOne({ email: req.body.email });
+        if (!userExists) {
+            return res.send({
+                message: 'User does not exist.',
+                success: false,
+                data: null,
+            });
+        }
+        const passwordMatch = await bcrypt.compare(req.body.password, userExists.password);
+        if (!passwordMatch) {
+            return res.send({
+                message: 'Incorrect password.',
+                success: false,
+                data: null,
+            });
+        }
+        const token = jwt.sign({ userId: userExists._id }, process.env.jwt_secret , { expiresIn: "1h" });
+
+        res.send({
+            message: 'User logged in successfully.',
+            success: true,
+            data: token,
+        });
+    }
+    catch (error) {
+        res.send({
+            message: 'An error occurred.',
+            success: false,
+            data: null,
+        })
+    }
 });
 
 module.exports = router;
