40edc6d99454f9e21b41d7dd83df33a7bff80a52
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-19 09:44:59 +0100

    login is working

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
+import { Form, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function login() {
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post("/api/users/login", values);
+      if (response.data.success) {
+        message.success(response.data.message);
+        localStorage.setItem("token", response.data.data);
+        navigate("/");
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      message.error(error.message);
+    }
+  };
   return (
-    <div>login</div>
-  )
+    <div className="flex justify-center">
+      <h2 className="text-4xl font-bold text-center text-black ">
+        Bienvenue à
+      </h2>
+      <h2 className="text-4xl font-bold text-center text-orange-500">
+        Supratours
+      </h2>
+      <Form layout="vertical" onFinish={onFinish}>
+        <Form.Item label="Email" name="email">
+          <input
+            type="email"
+            placeholder="test@gmail.com"
+            // onChange={(e) => setPhonenumber(e.target.value)}
+            className=" px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-orange-500 focus:bg-white focus:outline-none"
+            autoFocus
+            autoComplete="true"
+            required
+          />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+          <input
+            type="password"
+            placeholder="********"
+            // onChange={(e) => setPhonenumber(e.target.value)}
+            className=" px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-orange-500 focus:bg-white focus:outline-none"
+            autoFocus
+            autoComplete="true"
+            required
+          />
+        </Form.Item>
+
+        <button className="w-full block bg-orange-500 hover:bg-orange-400 focus:bg-orange-400 text-white font-semibold rounded-lg px-4 py-3 mt-6">
+          Register
+        </button>
+        <div className="flex justify-center items-center mt-6">
+          <Link
+            to="/register"
+            className="inline-block text-sm text-orange-500 align-baseline hover:text-orange-800"
+          >
+            Create an Account!
+          </Link>
+        </div>
+      </Form>
+    </div>
+  );
 }
 
-export default login
+export default login;
