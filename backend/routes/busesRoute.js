53d533f7c94b3c728cd868f6a381ccf266057c90
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-24 10:54:18 +0100

    update busesRouter

--- backend/routes/busesRoute.js
+++ backend/routes/busesRoute.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
+const authMiddlewares = require("../middlewares/authMiddlewares");
 const Bus = require("../models/busModel");
 
+
 //add bus
-router.post("/add-bus", async (req, res) => {
+router.post("/add-bus", async (req, res) => {   
     try {
-        const existingBus = await Bus.findOne({ number: req.body.number });
-        if (existingBus) {
+        const existingBus = await Bus.findOne({ number: req.body.number }); //check if bus already exists   
+        if (existingBus) {  //  if bus exists, return error
             return res.status(400).send({ success: false, message: "Bus already exists" });
         }
         const newBus = new Bus(req.body);
@@ -16,4 +18,16 @@ router.post("/add-bus", async (req, res) => {
     }
 });
 
-module.exports = router;
+//get all buses
+router.get("/get-all-buses", authMiddlewares, async (req, res) => {
+    try {
+        const buses = await Bus.find();
+        return res.status(200).send({ success: true, message: "Buses fetched successfully", data: buses });
+
+
+    } catch (error) {
+res.status(500).send({ success: false, message: "Buses are not defined" });
+    }
+
+});
+    module.exports = router;
