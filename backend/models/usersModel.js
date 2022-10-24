7969e3365cbcb3aaf118ace8eb3e9dcab64ddeb0
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-24 10:53:55 +0100

    update userModel

--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -20,4 +20,4 @@ const userSchema = new mongoose.Schema({
 
 },
     { timestamps: true });
-module.exports = mongoose.model('users', userSchema);
+module.exports = mongoose.model('users', userSchema); //
