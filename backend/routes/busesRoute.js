4d4540f3aa8fd26cf97211fb811d8ba191c693fa
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-26 09:17:08 +0100

    busRouter update

--- backend/routes/busesRoute.js
+++ backend/routes/busesRoute.js
@@ -3,8 +3,8 @@ const authMiddlewares = require("../middlewares/authMiddlewares");
 const Bus = require("../models/busModel");
 
 
-//add bus
-router.post("/add-bus", async (req, res) => {   
+//ad d bus
+router.post("/add-bus", async (req, res) => {
     try {
         const existingBus = await Bus.findOne({ number: req.body.number }); //check if bus already exists   
         if (existingBus) {  //  if bus exists, return error
@@ -18,6 +18,26 @@ router.post("/add-bus", async (req, res) => {
     }
 });
 
+//update bus
+router.get("/update-bus", authMiddlewares, async (req, res) => {
+    try {
+        await Bus.findByIdAndUpdate(req.body._id, req.body);
+        return res.status(200).send({ success: true, message: "Bus updated successfully" });
+    } catch (error) {
+        res.status(500).send({ success: false, message: error.message });
+    }
+});
+
+//delete bus
+router.post("/delete-bus", authMiddlewares, async (req, res) => {
+    try {
+        await Bus.findByIdAndDelete(req.body._id);
+        return res.status(200).send({ success: true, message: "Bus deleted successfully" });
+    } catch (error) {
+        res.status(500).send({ success: false, message: error.message });
+    }
+});
+
 //get all buses
 router.get("/get-all-buses", authMiddlewares, async (req, res) => {
     try {
@@ -26,8 +46,21 @@ router.get("/get-all-buses", authMiddlewares, async (req, res) => {
 
 
     } catch (error) {
-res.status(500).send({ success: false, message: "Buses are not defined" });
+        res.status(500).send({ success: false, message: "Buses are not defined" });
     }
 
 });
-    module.exports = router;
+
+//get bus by id
+router.post("/get-bus-by-id", authMiddlewares, async (req, res) => {
+    try {
+        const bus = await Bus.findById(req.body._id);
+        return res.status(200).send({ success: true, message: "Bus fetched successfully", data: bus });
+    } catch (error) {
+        res.status(500).send({ success: false, message: "Bus is not defined" });
+    }
+});
+
+
+
+module.exports = router;
