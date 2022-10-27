575859d5b9ba99911c5e5be8d9bf9cd61f6d8d5e
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-27 16:05:15 +0100

    Seat Selection update

--- client/src/components/SeatSelection.jsx
+++ client/src/components/SeatSelection.jsx
@@ -15,19 +15,21 @@ function SeatSelection({ selectedSeats, setSelectedSeats, bus }) {
     <div>
       <div className="bus-container w-[400px]">
         <Card>
-          <Row gutter={[0, 5]}>
+          <Row gutter={[0, 8]}>
             {Array.from(Array(capacity).keys()).map((seat) => {
               let seatClass = ``;
               selectedSeats.includes(seat + 1);
               if (selectedSeats.includes(seat + 1)) {
                 seatClass = `bg-orange-500 border-[2px] border-orange-500 text-center text-black text-base font-mono p-1 cursor-pointer w-10 h-10 rounded-tl-3xl rounded-br-3xl `;
               } else if (bus.seatsBooked.includes(seat + 1)) {
-                seatClass = `bg-black border-[2px] border-black text-center text-white text-base font-mono p-1 cursor-pointer w-10 h-10 rounded-tl-3xl rounded-br-3xl `;
+                seatClass = ` bg-gray-500 border-[2px] border-gray-500 text-center text-black text-base font-mono p-1 pointer-events-none cursor-not-allowed w-10 h-10 rounded-tl-3xl rounded-br-3xl `;
+
+                // seatClass = `bg-black border-[2px] border-black text-center text-white text-base font-mono p-1 cursor-pointer w-10 h-10 rounded-tl-3xl rounded-br-3xl `;
               }
               return (
                 <Col span={6}>
                   <div className="flex justify-center">
-                    <div className="border-[2px] border-gray-500 text-center text-black text-base font-mono p-1 cursor-pointer w-10 h-10 rounded-tl-3xl rounded-br-3xl ">
+                    <div className="border-[2px] border-gray-500 text-center text-black text-base font-mono p-1  w-10 h-10 rounded-tl-3xl rounded-br-3xl ">
                       <div
                         className={`seat ${seatClass}`}
                         onClick={() => selectOrUnselectSeats(seat + 1)}
