18ef495907f03f6c593f854e384d56d2f11f98a5
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-27 16:03:10 +0100

    index update

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,10 @@ app.use(express.json());
 
 const usersRoute = require('./routes/usersRoute');
 const busesRoute = require('./routes/busesRoute');
+const bookingsRoute = require('./routes/bookingsRoute');
 
 app.use('/api/users', usersRoute);
 app.use('/api/buses', busesRoute);
+app.use('/api/bookings', bookingsRoute);
 
 app.listen(port, () => console.log(`Node server Listening on port ${port}`));
