9de4ea5dcbe57adcfcd4f887f684e8331bf2b377
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-27 16:05:53 +0100

    Home update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import { Col, message, Row } from "antd";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Bus from "../components/Bus";
+import { axiosInstance } from "../helpers/axiosInstance";
+import { HideLoading, ShowLoading } from "../redux/alertsSlice";
 
 function Home() {
+  const { user } = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+  const [buses, setBuses] = useState([]);
 
-const {user} = useSelector(state => state.users);
+  const getBuses = async () => {
+    try {
+      dispatch(ShowLoading());
+      const response = await axiosInstance.get("/api/buses/get-all-buses", {});
+      dispatch(HideLoading());
+      if (response.data.success) {
+        setBuses(response.data.data);
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getBuses(); // eslint-disable-next-line
+  }, []);
 
   return (
-
+    <div>
+      <div></div>
       <div>
-          {user && <h1>Welcome {user?.name}</h1>}
+        <Row>
+          {buses.map((bus) => (
+            <Col lg={12} xs={24} sm={24}>
+            <Bus bus={bus} />
+            </Col>
+          ))}
+        </Row>
       </div>
-
-  )
+    </div>
+  );
 }
 
-export default Home
+export default Home;
