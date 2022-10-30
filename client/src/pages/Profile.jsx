2fae70306d4ede9852c91b745ec0f044a755dc0c
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-30 11:19:46 +0100

    update Profil page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,32 +4,7 @@ import { Navigate } from 'react-router-dom';
 function Profile() {
   return (
     <div>
-       <div className="sidebar w-[300px] h-full bg-gray-100">
-        <div className="logo flex justify-center items-center h-20">
-            
-            <div className="menu flex flex-col gap-2">
-                {menutoBeRendered.map((menu) => (
-                    <div
-                        className={`menu-item flex items-center gap-2 p-2 rounded-lg cursor-pointer 
-                            
-                        `} 
-                        onClick={() => {
-                            if (menu.path === "/logout") {
-                                localStorage.removeItem("token");
-                                Navigate("/login");
-                            } else {
-                                Navigate(menu.path);
-                            }
-                        }}
-                    >
-                        <i className={menu.icon}></i>
-                        <span>{menu.name}</span>
-                        <i className="ri-arrow-right-s-line"></i>
-                        </div> 
-                ))}
-                </div>
-                </div>
-                </div>
+        <h1>Profile</h1>
                         
     </div>
   )
