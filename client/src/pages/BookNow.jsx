4b23627540a1b88262c6f26f56ac839ff6b21d34
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-30 11:18:42 +0100

    update book Now

--- client/src/pages/BookNow.jsx
+++ client/src/pages/BookNow.jsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import SeatSelection from "../components/SeatSelection";
 import { axiosInstance } from "../helpers/axiosInstance";
 import { HideLoading, ShowLoading } from "../redux/alertsSlice";
+import StripeCheckout from "react-stripe-checkout";
 
 function BookNow() {
   const [selectedSeats, setSelectedSeats] = useState([]);
@@ -29,16 +30,36 @@ function BookNow() {
     }
   };
 
-const bookNow = async () => {
+  const bookNow = async (transactionId) => {
     try {
       dispatch(ShowLoading());
       const response = await axiosInstance.post("/api/bookings/book-seat", {
         bus: bus._id,
         seats: selectedSeats,
+        transactionId,
       });
       dispatch(HideLoading());
       if (response.data.success) {
         message.success(response.data.message);
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+  const onToken = async (token) => {
+    try {
+      dispatch(ShowLoading());
+      const response = await axiosInstance.post("/api/bookings/make-payment", {
+        token,
+        amount: selectedSeats.length * bus.price * 100,
+      });
+      dispatch(HideLoading());
+      if (response.data.success) {
+        message.success(response.data.message);
+        bookNow(response.data.data.transactionId);
       } else {
         message.error(response.data.message);
       }
@@ -86,41 +107,57 @@ const bookNow = async () => {
                 <i class="ri-creative-commons-by-fill"></i> Bus seats <br />{" "}
                 {bus.seats}
               </h1>
-             
             </div>
           </Col>
           <Col lg={8} xs={12} sm={12}>
-          <div className="text-xl flex flex-col  mt-[250px] items-start ml-[300px] ">
+            <div className="border-[1px] border-orange-400 mt-10"></div>
+
+            <div className="text-xl flex flex-row  justify-center mt-[250px] items-start  gap-10">
               <h1 className="text-center">
-                <i class="ri-creative-commons-by-fill"></i> Seats Left{" "}
-                <br /> {bus.seats - bus.seatsBooked.length}
+                <i class="ri-creative-commons-by-fill"></i> Seats Left <br />{" "}
+                {bus.seats - bus.seatsBooked.length}
               </h1>
-          <h1 className="text-center mt-5">
+              {/* <h1 className="text-center mt-5">
                 <i class="ri-creative-commons-by-fill"></i> Bus Seats Selected  {" "}
                 <br /> {selectedSeats.join("/ ")}
+              </h1> */}
+              <h1 className="text-center ">
+                <i class="ri-money-dollar-circle-line"></i> Total price <br />{" "}
+                {selectedSeats.length * bus.price} DH
               </h1>
-              <h1 className="text-center mt-5">
-              <i class="ri-money-dollar-circle-line"></i> Total price{" "}
-                <br /> {selectedSeats.length * bus.price} DH
-              </h1>
+            </div>
+            <StripeCheckout
+              billingAddress
+              disabled={selectedSeats.length === 0}
+              token={onToken}
+              amount={bus.price * selectedSeats.length * 100}
+              currency="MAD"
+              stripeKey="pk_test_51Ky11OFDbGl4ER5ateVIT59tNNQ6YARPNORBfr4ozDHniOg1n8Y28djsmYRR2xg15ygMbmvihmQwLRVpfkSz3c7z00mLLm8FRR"
+            >
+              <div className="flex justify-center mt-3">
+                <button
+                  className={`${
+                    selectedSeats.length === 0
+                      ? "bg-gray-400 cursor-not-allowed "
+                      : "bg-orange-400 text-white px-5 py-2 rounded-md"
+                  } text-white px-10 py-2 rounded-lg`}
+                >
+                  Book Now <i class="ri-arrow-right-s-line"></i>
+                </button>
               </div>
+            </StripeCheckout>
           </Col>
+
           <Col lg={16} xs={12} sm={12}>
-            <div className="flex justify-center mt-10 ">
-              <SeatSelection 
+            <div className="border-[1px] border-orange-400 mt-10"></div>
+            <div className="flex justify-center mt-5 ">
+              <SeatSelection
                 selectedSeats={selectedSeats}
                 setSelectedSeats={setSelectedSeats}
                 bus={bus}
                 // seatsBooked={bus.seatsBooked}
               />
             </div>
-          </Col>
-          <Col lg={24} xs={24} sm={24}>
-            <div className="flex justify-center mt-3">
-              <button className="bg-orange-400 text-white px-10 py-2 rounded-md" onClick={bookNow}>
-                Book Now
-              </button>
-              </div>
           </Col>
         </Row>
       )}
