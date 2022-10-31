6e864b93fe5ab3739026261622bac3a425c728c0
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-31 16:11:40 +0100

    update Booking

--- client/src/pages/Bookings.jsx
+++ client/src/pages/Bookings.jsx
@@ -4,10 +4,14 @@ import { useDispatch } from "react-redux";
 import BusForm from "../components/BusForm";
 import PageTitle from "../components/PageTitle";
 import { HideLoading, ShowLoading } from "../redux/alertsSlice";
-import { message, Table } from "antd";
+import { message, Modal, Table } from "antd";
 import { axiosInstance } from "../helpers/axiosInstance";
 
 function Bookings() {
+  //cancel booking
+ 
+  const [showPrintModal, setShowPrintModal] = useState(false);
+  const [selectedBooking, setSelectedBooking] = useState(null);
   const [bookings, setBookings] = useState([]);
   const dispatch = useDispatch();
   const getBookings = async () => {
@@ -37,6 +41,33 @@ function Bookings() {
     }
   };
 
+  const CancelBooking = async () => {
+      try {
+        dispatch(ShowLoading());
+        const res = await axiosInstance.get(
+          `/api/bookings/${localStorage.getItem("user_id")}`
+        );
+        const bus_id = res.data.data[0].bus._id;
+        const user_id = res.data.data[0].user._id;
+        const booking_id = res.data.data[0]._id;
+        const response = await axiosInstance.delete(
+          `/api/bookings/${booking_id}/${user_id}/${bus_id}`,
+          {}
+        );
+        dispatch(HideLoading());
+        if (response.data.success) {
+          message.success(response.data.message);
+          getBookings();
+        }else{
+          message.error(response.data.message);
+        }
+      } catch (error) {
+        dispatch(HideLoading());
+        message.error(error.message);
+
+      }
+  };
+
   const columns = [
     {
       title: "Passenger Name",
@@ -47,15 +78,18 @@ function Bookings() {
       title: "Bus Name & Number",
       dataIndex: "name,number",
       key: "name,number",
-      render: (text, record) => (
-        <span>
-          {record.bus.name} / N°{record.bus.number}
-        </span>
-      ),
+      render: (text, record) => {
+        return `${record.bus.name} - ${record.number}`;
+      },
     },
     {
-      title: "",
-     
+      //seatsBooked
+      title: "Seats Booked",
+      dataIndex: "seatsBooked",
+      key: "seatsBooked, seatsAvailable",
+      render: (text, record) => {
+        return `${record.seatsBooked} / ${record.seatsAvailable}`;
+      },
     },
     {
       title: "From",
@@ -67,37 +101,37 @@ function Bookings() {
       dataIndex: "to",
       key: "to",
     },
-
-    {
-      title: "Price",
-      dataIndex: "price",
-      key: "price",
-      render: (price) => price,
-    },
     {
       title: "Date",
       dataIndex: "date",
-      // key: "transactionId",
+      key: "transactionId",
     },
     {
-      title: "From",
-      dataIndex: "from",
+      title: "Depart Time",
+      dataIndex: "departureTime",
+      key: "departureTime",
     },
     {
-      title: "To",
-      dataIndex: "to",
+      //tottal price
+      title: "Total Price",
+      dataIndex: "price",
+      key: "price",
+      render: (bus, record) => {
+        return `${record.price * record.selectedSeats} DH`;
+      },
     },
+
     {
       title: "Status",
       dataIndex: "status",
       key: "status",
       render: (status) => {
-        if (status === "pending") {
-          return <span className="text-warning">{status}</span>;
+        if (status === "Yet to start") {
+          return <span className="text-orange-600">{status}</span>;
         } else if (status === "cancelled") {
-          return <span className="text-danger">{status}</span>;
+          return <span className="text-red-600	">{status}</span>;
         } else {
-          return <span className="text-success">{status}</span>;
+          return <span className="text-green-600	">{status}</span>;
         }
       },
     },
@@ -106,12 +140,29 @@ function Bookings() {
       dataIndex: "action",
       key: "action",
       render: (text, record) => (
-        <button
-          className="btn btn-danger"
-          //   onClick={() => cancelBooking(record._id)}
-        >
-          Cancel
-        </button>
+        <div className="flex gap-4">
+          <span>
+            <button
+              className="bg-red-500 text-white px-2 py-1 rounded"
+              onClick={() => {
+                CancelBooking();
+              }}
+            >
+              Cancel
+            </button>
+          </span>
+          <span>
+            <button
+              className="bg-green-500 text-white px-2 py-1 rounded"
+              onClick={() => {
+                setSelectedBooking(record);
+                setShowPrintModal(true);
+              }}
+            >
+              Print Ticket
+            </button>
+          </span>
+        </div>
       ),
     },
   ];
@@ -124,7 +175,119 @@ function Bookings() {
     <div>
       <PageTitle title="Bookings" />
       <Table dataSource={bookings} columns={columns} />
-    </div>
+      {showPrintModal && (
+        <Modal
+          title="Print Ticket"
+          onCancel={() => {
+            setShowPrintModal(false);
+            setSelectedBooking(null);
+          }}
+          visible={showPrintModal}
+          footer={null}
+          width={700}
+        >
+          <h1 className="text-lg flex justify-center">
+            This Your Ticket -{selectedBooking?.name}
+          </h1>
+          <hr />
+          <div className="flex justify-between mt-4">
+            <p className="flex justify-center">
+              <span className="font-bold">Bus Name / Bus Number :</span>{" "}
+              {selectedBooking?.bus.name} / {selectedBooking?.number}
+            </p>
+            <p className="flex justify-center">
+              <span className="font-bold">From - To :</span>{" "}
+              {selectedBooking?.from} - {selectedBooking?.to}
+            </p>
+            <p className="flex justify-center">
+              <span className="font-bold">Date :</span> {selectedBooking?.date}
+            </p>
+          </div>
+          <div className="flex justify-between mt-4">
+            <p className="flex justify-center">
+              <span className="font-bold">Depart Time :</span>{" "}
+              {selectedBooking?.departureTime}
+            </p>
+            <p className="flex justify-center">
+              <span className="font-bold">Seats Booked :</span>{" "}
+              {selectedBooking?.seatsBooked}
+            </p>
+            <p className="flex justify-center">
+              <span className="font-bold">Total Price :</span>{" "}
+              {selectedBooking?.price} DH
+            </p>
+          </div>
+          <div className="flex justify-between mt-4 mx-20">
+            <p className="flex justify-center">
+              <span className="font-bold ">Status :</span>{" "}
+              <span className="text-orange-600">{selectedBooking.status}</span>
+            </p>
+            <p className="flex justify-center">
+              <span className="font-bold">Passenger Email :</span>{" "}
+              {selectedBooking?.email}
+            </p>
+          </div>
+          <p className="flex justify-center">
+            <span className="font-bold">Free Wifi :<span><i class="ri-arrow-down-s-fill"></i></span> </span>
+          </p>
+          
+   <div className="text-center flex justify-center">
+            <img
+              alt="Barcode Generator TEC-IT"
+              src="https://barcode.tec-it.com/barcode.ashx?data=WIFI%3AT%3AWPA%3BS%3AYoucode1%3BP%3AYcode%402021%3B%3B&code=QRCode_Wifi&dmsize=Default&eclevel=L"
+            />
+          </div>
+         
+           <div className="pt-[8px] text-center font-serif">
+          
+          </div>
+          <div className="flex justify-center mt-4">
+            <button
+              className="bg-green-500 text-white px-2 py-1 rounded"
+              onClick={() => {
+                window.print();
+              }}
+            >
+              Print
+            </button>
+          </div>       
+        </Modal>
+          )}
+          {/* {cancelBooking && (
+            <Modal
+              title="Cancel Booking"
+              onCancel={() => {
+                setCancelBooking(false);
+                setSelectedBooking(null);
+              }}
+              visible={cancelBooking}
+              footer={null}
+              width={700}
+            >
+              <h1 className="text-lg flex justify-center">
+                Are you sure you want to cancel this booking?
+              </h1>
+              <hr />
+              <div className="flex justify-between mt-4">
+                <p className="flex justify-center">
+                  <span className="font-bold">Bus Name / Bus Number :</span>{" "}
+                  {selectedBooking?.bus.name} / {selectedBooking?.number}
+                </p>
+                <p className="flex justify-center">
+                  <span className="font-bold">From - To :</span>{" "}
+                  {selectedBooking?.from} - {selectedBooking?.to}
+                </p>
+                <p className="flex justify-center">
+                  <span className="font-bold">Date :</span>{" "}
+                  {selectedBooking?.date}
+                </p>
+              </div>
+                </Model>
+              )} */}
+
+        </div>
+
+    
   );
 }
 
