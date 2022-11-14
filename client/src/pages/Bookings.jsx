c8520b6ce427bd65e3df378adc1bb62c9b4ac608
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-11-15 00:29:31 +0100

    when client reserve two or more , every seat have owned ticket

--- client/src/pages/Bookings.jsx
+++ client/src/pages/Bookings.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import PageTitle from "../components/PageTitle";
 import { HideLoading, ShowLoading } from "../redux/alertsSlice";
 import { message, Modal, Table } from "antd";
 import { axiosInstance } from "../helpers/axiosInstance";
+import moment from "moment";
+import { useReactToPrint } from "react-to-print";
+import QRCode from "react-qr-code";
 
 function Bookings() {
   const [showPrintModal, setShowPrintModal] = useState(false);
@@ -77,9 +80,9 @@ function Bookings() {
       //seatsBooked
       title: "Seats Booked",
       dataIndex: "seatsBooked",
-      key: "seatsBooked, seatsAvailable",
+      key: "seatsBooked",
       render: (text, record) => {
-        return `${record.seatsBooked} / ${record.seatsAvailable}`;
+        return `${record.seatsBooked}`;
       },
     },
     {
@@ -103,15 +106,14 @@ function Bookings() {
       key: "departureTime",
     },
     {
-      //tottal price
+      //total price
       title: "Total Price",
       dataIndex: "price",
       key: "price",
-      render: (bus, record) => {
-        return `${record.price * record.selectedSeats} DH`;
+      render: (text, record) => {
+        return `${record.price * record.seatsBooked.length}`;
       },
     },
-
     {
       title: "Status",
       dataIndex: "status",
@@ -156,98 +158,171 @@ function Bookings() {
     getBookings(); // eslint-disable-next-line
   }, []);
 
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
   return (
     <div className="">
       <PageTitle title="Bookings" />
-      <Table 
-      className="table-responsive"
-      dataSource={bookings} columns={columns} />
+      <Table dataSource={bookings} columns={columns} />
       {showPrintModal && (
         <Modal
+          width={1000}
+          height={500}
           title="Print Ticket"
           onCancel={() => {
             setShowPrintModal(false);
             setSelectedBooking(null);
           }}
-          visible={showPrintModal}
-          footer={null}
-          width={1000}
+          open={showPrintModal}
+          okText="Print"
+          onOk={handlePrint}
         >
-          <div className="">
-            <div className="flex justify-center">
-              <img
-                alt="Barcode Generator TEC-IT"
-                src="https://barcode.tec-it.com/barcode.ashx?code=Code128&multiplebarcodes=true&translate-esc=true&data=6356ab6efb7652302d19dd91&eclevel=L"
-              />
-            </div>
+          <div className="" ref={componentRef}>
+            {selectedBooking &&
+              selectedBooking.seatsBooked.map((seat) => {
+                return (
+                  <div className=" max-w-md w-full h-full mx-auto z-10 bg-orange-600 ">
+                    <div className="flex flex-col">
+                      <div className="bg-white relative drop-shadow-2xl rounded-3xl p-4 m-4">
+                        <div className="flex-none sm:flex">
+                          <div className="flex-auto justify-evenly">
+                            <div className="flex items-center justify-between">
+                              <div className="flex items-center  my-1">
+                                <h2 className="font-medium">IL-MEGLIO</h2>
+                              </div>
+                              <div className="flex justify-between ml-auto">
+                               
+                              </div>
+                              <div className="ml-auto text-orange-800">
+                                {selectedBooking?.bus.name} /{" "}
+                                {selectedBooking?.number}
+                              </div>
+                            </div>
+                            <div className="border-dashed border-b-2 my-5" />
+                            <div className="flex items-center">
+                              <div className="flex flex-col justify-start text-center">
+                                <div className="flex-auto text-xs text-gray-400 my-1">
+                                  <span className="mr-1 ">From</span>
+                                  <span></span>
+                                </div>
+                                <div className="w-full flex-none text-lg text-orange-800 font-bold leading-none">
+                                  {selectedBooking?.from}
+                                </div>
+                              </div>
+                              <div className="flex flex-col mx-auto">
+                                <img
+                                  src="https://www.pngitem.com/pimgs/m/94-947302_png-bus-ticket-traveling-bus-logo-transparent-png.png"
+                                  className="w-20 p-1"
+                                />
+                              </div>
+                              <div className="flex flex-col justify-end text-end">
+                                <div className="flex-auto text-xs text-gray-400 my-1">
+                                  <span className="mr-1">To</span>
+                                </div>
+                                <div className="w-full flex-none text-lg text-orange-800 font-bold leading-none">
+                                  {selectedBooking?.to}
+                                </div>
+                              </div>
+                            </div>
+                            <div className="border-dashed border-b-2 my-5 pt-5">
+                              <div className="absolute rounded-full w-5 h-5 bg-orange-600 -mt-2 -left-1" />
+                              <div className="absolute rounded-full w-5 h-5 bg-orange-600 -mt-2 -right-1" />
+                            </div>
+                            <div className="flex items-center mb-5 p-5 text-sm">
+                              <div className="flex flex-col text-start">
+                                <span className="text-sm">Price</span>
+                                <span className="font-semibold">
+                                  {selectedBooking.price *
+                                    selectedBooking.seatsBooked.length}{" "}
+                                  /Dh
+                                </span>
+                              </div>
+                              <div className="flex flex-col ml-auto text-end">
+                                <span className>Date</span>
+                                <div className="font-semibold">
+                                  {selectedBooking?.date}
+                                </div>
+                              </div>
+                            </div>
+                            <div className="flex items-center mb-5 p-5 text-sm">
+                              <div className="flex flex-col text-sm">
+                                <span className>Departs</span>
+                                <div className="font-semibold">
+                                  {selectedBooking?.departureTime}
+                                </div>
+                              </div>
 
-            <hr />
-            <div className="flex justify-between mt-4">
-              <p className="flex justify-center">
-                <span className="font-bold">Bus Name / Bus Number :</span>{" "}
-                {selectedBooking?.bus.name} / {selectedBooking?.number}
-              </p>
-              <p className="flex justify-center">
-                <span className="font-bold">From - To :</span>{" "}
-                {selectedBooking?.from} - {selectedBooking?.to}
-              </p>
-              <p className="flex justify-center">
-                <span className="font-bold">Date :</span>{" "}
-                {selectedBooking?.date}
-              </p>
-            </div>
-            <div className="flex justify-between mt-4">
-              <p className="flex justify-center">
-                <span className="font-bold">Depart Time :</span>{" "}
-                {selectedBooking?.departureTime}
-              </p>
-              <p className="flex justify-center">
-                <span className="font-bold">Seats Booked: </span>{" "}
-                {selectedBooking?.seatsBooked}
-              </p>
-              <p className="flex justify-center">
-                <span className="font-bold">Total Price :</span>{" "}
-                {selectedBooking?.price} DH
-              </p>
-              <p className="flex justify-center">
-                <span className="font-bold ">Status :</span>{" "}
-                <span className="text-orange-600">
-                  {selectedBooking.status}
-                </span>
-              </p>
-            </div>
-
-            <p className="flex justify-center">
-              <span className="font-bold">
-                Free Wifi :
-                <span>
-                  <i class="ri-arrow-down-s-fill"></i>
-                </span>{" "}
-              </span>
-            </p>
-
-            <div className="text-center flex justify-center">
-              <img
-                alt="Barcode Generator TEC-IT"
-                src="https://barcode.tec-it.com/barcode.ashx?data=WIFI%3AT%3AWPA%3BS%3AYoucode1%3BP%3AYcode%402021%3B%3B&code=QRCode_Wifi&dmsize=Default&eclevel=L"
-              />
-            </div>
-
-            <div className="pt-[8px] text-center font-serif"></div>
-            <div className="flex justify-center mt-4">
-              <button
-                className=" text-black px-2 py-1 rounded"
-                onClick={() => {
-                  if (window.print) {
-                    document.write(
-                      '<input type="button" value="Sent to Printer" id="btnRead" class="btn btn-warning" onClick="window.print()">'
-                    );
-                  }
-                }}
-              >
-                <i class="ri-printer-line"></i>
-              </button>
-            </div>
+                              <div className="flex flex-col ml-auto text-end">
+                                <span className>Arrived</span>
+                                <div className="font-semibold">
+                                  {selectedBooking?.arrivalTime}
+                                </div>
+                              </div>
+                            </div>
+                            <div className="border-dashed border-b-2 my-5 pt-5">
+                              <div className="absolute rounded-full w-5 h-5 bg-orange-600 -mt-2 -left-1" />
+                              <div className="absolute rounded-full w-5 h-5 bg-orange-600 -mt-2 -right-1" />
+                            </div>
+                            <div className="flex items-center px-5 pt-3 text-sm">
+                              <div className="flex flex-col">
+                                <span className>Passanger</span>
+                                <div className="font-semibold">
+                                  {selectedBooking?.name}
+                                </div>
+                              </div>
+                              <div className="flex flex-col mx-auto">
+                                <span className>Class</span>
+                                <div className="font-semibold">
+                                  {selectedBooking?.type}
+                                </div>
+                              </div>
+                              <div className="flex flex-col">
+                                <span className>Seats</span>
+                                <div className="font-semibold">{seat}</div>
+                              </div>
+                            </div>
+                            <div className="flex flex-col py-5  justify-center text-sm ">
+                              <p className="flex justify-center">
+                                <span className="font-bold">
+                                  Scane Me Free Wifi
+                                </span>
+                              </p>
+                              <span className="flex justify-center">
+                                <i class="ri-arrow-down-s-fill"></i>
+                              </span>{" "}
+                              <div className="text-center flex justify-center">
+                              <QRCode
+                                  value={JSON.stringify({
+                                    Name: selectedBooking?.name,
+                                    From: selectedBooking?.from,
+                                    To: selectedBooking?.to,
+                                    Departure: selectedBooking?.departureTime,
+                                    Arrival: selectedBooking?.arrivalTime,
+                                    Price:
+                                      selectedBooking.price *
+                                      selectedBooking.seatsBooked.length,
+                                    Date: selectedBooking?.date,
+                                    seat,
+                                  })}
+                                  size={100}
+                                  bgColor={"#9a3412"}
+                                  fgColor={"#ffffff"}
+                                  level={"L"}
+                                  includeMargin={true}
+                                />
+                              </div>
+                              <p className="flex justify-center"></p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
           </div>
         </Modal>
       )}
