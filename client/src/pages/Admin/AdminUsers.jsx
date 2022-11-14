4b3a4fe5325c3c5bde774e47a2b58bfad62e977f
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-11-15 00:27:21 +0100

    block & unblock & remove admin & make admin

--- client/src/pages/Admin/AdminUsers.jsx
+++ client/src/pages/Admin/AdminUsers.jsx
@@ -1,65 +1,202 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-
-import { message, Modal, Table } from "antd";
+import PageTitle from "../../components/PageTitle";
 import { HideLoading, ShowLoading } from "../../redux/alertsSlice";
+import { message, Table } from "antd";
 import { axiosInstance } from "../../helpers/axiosInstance";
+import axios from "axios";
 
 function AdminUsers() {
-//get all users
-const [users , setUsers]= useState
-const dispatch = useDispatch
-const getUsers = async () => {
-  try {
-    dispatch(ShowLoading());
-    const response = await axiosInstance.get("/api/users/get-all-users", {});
-    dispatch(HideLoading());
-    if (response.data.success) {
-      setUsers(response.data.data);
-    } else {
-      message.error(response.data.message);
-    }
-  } catch (error) {
-    dispatch(HideLoading());
-    message.error(error.message);
-  }
-};
+  const dispatch = useDispatch();
 
-  
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    try {
+      dispatch(ShowLoading());
+      const response = await axios.get("/api/users/get-all-users", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      dispatch(HideLoading());
+      if (response.data.success) {
+        setUsers(response.data.data);
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
+  //Block User
+  // const blockUser = async (id) => {
+  //   try {
+  //     dispatch(ShowLoading());
+  //     const response = await axiosInstance.post(`/api/users/block-user/${id}`, {
+  //       //id
+  //       // bookings_id = ,
+  //       data: { id },
+  //     });
+  //     dispatch(HideLoading());
+  //     if (response.data.success) {
+  //       message.success(response.data.message);
+  //       getUsers();
+  //     } else {
+  //       message.error(response.data.message);
+  //     }
+  //   } catch (error) {
+  //     dispatch(HideLoading());
+  //     message.error(error.message);
+  //   }
+  // };
+
+  //Unblock User
+  // const unblockUser = async (id) => {
+  //   try {
+  //     dispatch(ShowLoading());
+  //     const response = await axiosInstance.post(
+  //       `/api/users/unblock-user/${id}`,
+  //       {
+  //         //id
+  //         // bookings_id = ,
+  //         data: {},
+  //       }
+  //     );
+  //     dispatch(HideLoading());
+  //     if (response.data.success) {
+  //       message.success(response.data.message);
+  //       getUsers();
+  //     } else {
+  //       message.error(response.data.message);
+  //     }
+  //   } catch (error) {
+  //     dispatch(HideLoading());
+  //     message.error(error.message);
+  //   }
+  // };
+
+  const updateUserPermissions = async (user, action) => {
+    try {
+      let payload = null;
+      if (action === "make-admin") {
+        payload = {
+          ...user,
+          isAdmin: true,
+        };
+      } else if (action === "remove-admin") {
+        payload = {
+          ...user,
+          isAdmin: false,
+        };
+      } else if (action === "block") {
+        payload = {
+          ...user,
+          isBlocked: true,
+        };
+      } else if (action === "unblock") {
+        payload = {
+          ...user,
+          isBlocked: false,
+        };
+      }
+
+      dispatch(ShowLoading());
+      const response = await axiosInstance.post(
+        "/api/users/update-user-permissions",
+        payload
+      );
+      dispatch(HideLoading());
+      if (response.data.success) {
+        message.success(response.data.message);
+        getUsers();
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
   const columns = [
     {
-      title: "Name",
+      title: "Bus Name",
       dataIndex: "name",
     },
     {
       title: "Email",
       dataIndex: "email",
     },
-   
+    {
+      title: "Status",
+      dataIndex: "",
+      render: (data) => {
+        return data.isBlocked ? "Bloked" : "Avtive";
+      }
+    },
     {
       title: "Role",
-      dataIndex: "role",
+      dataIndex: "",
+      render: (data) => {
+        if (data.isAdmin) {
+          return "Admin";
+        } else {
+          return "User";
+        }
+      },
     },
+
     {
       title: "Action",
       dataIndex: "action",
-      render: (text, record) => (
-        <div>
-          <button
-            className="btn btn-danger"
-            onClick={() => {
-              // deleteUser(record._id);
-            }
-            }
-          >
-            Delete
-          </button>
+      render: (action, record) => (
+        <div className="flex gap-3 underline">
+          {record?.isBlocked && (
+            <p
+              className="underline cursor-pointer"
+              onClick={() => {
+                updateUserPermissions(record, "unblock");
+              }}
+            >
+              UnBlock
+            </p>
+          )}
+          {!record?.isBlocked && (
+            <p
+              className="underline cursor-pointer hover:text-red-600"
+              onClick={() => {
+                updateUserPermissions(record, "block");
+              }}
+            >
+              Block
+            </p>
+          )}
+          {record?.isAdmin && (
+            <p
+              className="underline cursor-pointer hover:text-green-600"
+              onClick={() => {
+                updateUserPermissions(record, "remove-admin");
+              }}
+            >
+              Remove Admin
+            </p>
+          )}
+          {!record?.isAdmin && (
+            <p
+              className="underline cursor-pointer"
+              onClick={() => {
+                updateUserPermissions(record, "make-admin");
+              }}
+            >
+              Make Admin
+            </p>
+          )}
         </div>
       ),
-            
-
-
     },
   ];
 
@@ -67,20 +204,18 @@ const getUsers = async () => {
     getUsers(); // eslint-disable-next-line
   }, []);
 
-
   return (
     <div>
-
+      <div className="flex justify-between">
+        <PageTitle title="Users" />
+      </div>
       <Table
         columns={columns}
         dataSource={users}
-        rowKey={(record) => record._id}
+        pagination={{ pageSize: 10 }}
       />
-
-
-
     </div>
   );
 }
 
-export default AdminUsers
+export default AdminUsers;
