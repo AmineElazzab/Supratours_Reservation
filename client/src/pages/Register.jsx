d2287fa5213ee19b1bb8ba69094828333949967a
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-11-15 17:37:13 +0100

    update register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -28,64 +28,6 @@ function Register() {
   };
 
   return (
-    // <div className="flex justify-center">
-    //     <h2 className="text-4xl font-bold text-center text-black ">
-    //       Bienvenue à
-    //     </h2>
-    //     <h2 className="text-4xl font-bold text-center text-orange-500">
-    //       Supratours
-    //     </h2>
-    //   <Form layout="vertical" onFinish={onFinish}>
-    //     <Form.Item label="Name"  name="name">
-    //       <input
-    //         type="text"
-
-    //         placeholder="joe doe"
-    //         // onChange={(e) => setPhonenumber(e.target.value)}
-    //         className=" px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-orange-500 focus:bg-white focus:outline-none"
-    //         autoFocus
-    //         autoComplete="true"
-    //         required
-    //       />
-    //     </Form.Item>
-    //     <Form.Item label="Email" name="email">
-    //       <input
-    //         type="email"
-
-    //         placeholder="test@gmail.com"
-    //         // onChange={(e) => setPhonenumber(e.target.value)}
-    //         className=" px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-orange-500 focus:bg-white focus:outline-none"
-    //         autoFocus
-    //         autoComplete="true"
-    //         required
-    //       />
-    //     </Form.Item>
-    //     <Form.Item label="Password" name="password" >
-    //       <input
-    //         type="password"
-
-    //         placeholder="********"
-    //         // onChange={(e) => setPhonenumber(e.target.value)}
-    //         className=" px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-orange-500 focus:bg-white focus:outline-none"
-    //         autoFocus
-    //         autoComplete="true"
-    //         required
-    //       />
-    //     </Form.Item>
-
-    //     <button className="w-full block bg-orange-500 hover:bg-orange-400 focus:bg-orange-400 text-white font-semibold rounded-lg px-4 py-3 mt-6">
-    //       Register
-    //     </button>
-    //     <div className="flex justify-center items-center mt-6">
-    //       <Link
-    //         to="/login"
-    //         className="inline-block text-sm text-orange-500 align-baseline hover:text-orange-800"
-    //       >
-    //         Already have an account? Login!
-    //       </Link>
-    //     </div>
-    //   </Form>
-    // </div>
     <div
       className="bg-no-repeat bg-cover bg-center w-screen h-screen"
       style={{
@@ -121,6 +63,15 @@ function Register() {
                     placeholder="test@gmail.com"
                     className=" px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-orange-500 focus:bg-white focus:outline-none"
                     
+                    autoComplete="true"
+                    required
+                  />
+                </Form.Item>
+                <Form.Item label="Phone" name="phone">
+                  <input
+                    type="number"
+                    placeholder="06 00 00 00 00"
+                    className=" px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-orange-500 focus:bg-white focus:outline-none "
                     autoComplete="true"
                     required
                   />
