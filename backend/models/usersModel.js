675bf5a106e8afff7b8ea8f7fcd8862d44d9eee0
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-11-15 17:27:45 +0100

    add phone number

--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -10,6 +10,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    phone :{
+        type: String,
+        required: true,
+        unique: true
+    },
     password: {
         type: String,
         required: true
