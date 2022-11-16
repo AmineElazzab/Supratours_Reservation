3d8d314646cdfb674e9cfb84b0969181aabf5d72
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-11-16 11:16:34 +0100

    update index page

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Image from "../assets/ticket.png"
+import Image from "../assets/ticket.jpg";
 
 // components
 
@@ -17,7 +17,7 @@ export default function Landing() {
             className="absolute top-0 w-full h-screen bg-center bg-cover"
             style={{
               backgroundImage:
-              "url('https://images2.alphacoders.com/671/thumb-1920-671500.jpg')",
+                "url('https://images2.alphacoders.com/671/thumb-1920-671500.jpg')",
             }}
           >
             <span
@@ -28,18 +28,18 @@ export default function Landing() {
               <div className="mockup-phone ">
                 <div className="camera"></div>
                 <div className="display">
-                  <div className="artboard artboard-demo phone-1 bg-cover bg-no-repeat	"
-                  style={{
-                    backgroundImage:
-                      "url(https://i.pinimg.com/236x/a7/d8/b5/a7d8b55277aa17c40fce3cfe9c13e3f5.jpg)",
-                  }}
+                  <div
+                    className="artboard artboard-demo phone-1 bg-cover bg-no-repeat	"
+                    // style={{
+                    //   backgroundImage:
+                    //     "url(https://i.pinimg.com/236x/a7/d8/b5/a7d8b55277aa17c40fce3cfe9c13e3f5.jpg)",
+                    // }}
                   >
-                    {/* <img src={Image} alt="ticket" className="w-full h-full" /> */}
-                  
+                    <img src={Image} alt="ticket" className="w-full h-full" />
                   </div>
                 </div>
               </div>
-              </div>
+            </div>
           </div>
 
           <div
@@ -63,7 +63,7 @@ export default function Landing() {
           </div>
         </div>
 
-        <section className="pb-20 bg-blueGray-200 -mt-24">
+        {/* <section className="pb-20 bg-blueGray-200 -mt-24">
           <div className="container mx-auto px-4">
             <div className="flex flex-wrap">
               <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
@@ -150,12 +150,164 @@ export default function Landing() {
                 </div>
               </div>
             </div>
-
-            
           </div>
-        </section>
+        </section> */}
       </main>
-      <Footer />
+      {/* component */}
+      <div
+        id="team"
+        className="section relative pt-20 pb-8 md:pt-16 bg-white dark:bg-gray-800"
+      >
+        <div className="container xl:max-w-6xl mx-auto px-4">
+          {/* section header */}
+          <header className="text-center mx-auto mb-12">
+            <h2 className="text-2xl leading-normal mb-2 font-bold text-gray-800 dark:text-gray-100">
+              <span className="font-light">Creat</span> By
+            </h2>
+            <svg
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              x="0px"
+              y="0px"
+              viewBox="0 0 100 60"
+              style={{ margin: "0 auto", height: "35px" }}
+              xmlSpace="preserve"
+            >
+              <circle
+                cx="50.1"
+                cy="30.4"
+                r={5}
+                className="stroke-primary"
+                style={{
+                  fill: "transparent",
+                  strokeWidth: 2,
+                  strokeMiterlimit: 10,
+                }}
+              />
+              <line
+                x1="55.1"
+                y1="30.4"
+                x2={100}
+                y2="30.4"
+                className="stroke-primary"
+                style={{ strokeWidth: 2, strokeMiterlimit: 10 }}
+              />
+              <line
+                x1="45.1"
+                y1="30.4"
+                x2={0}
+                y2="30.4"
+                className="stroke-primary"
+                style={{ strokeWidth: 2, strokeMiterlimit: 10 }}
+              />
+            </svg>
+          </header>
+          {/* end section header */}
+          {/* row */}
+          <div className="flex flex-wrap flex-row -mx-4 justify-center">
+            <div className="flex-shrink max-w-full px-4 w-2/3 sm:w-1/2 md:w-5/12 lg:w-1/4 xl:px-6">
+              <div
+                className="relative overflow-hidden bg-white dark:bg-gray-800 mb-12 hover-grayscale-0 wow fadeInUp"
+                data-wow-duration="1s"
+                style={{
+                  visibility: "visible",
+                  animationDuration: "1s",
+                  animationName: "fadeInUp",
+                }}
+              >
+                {/* team block */}
+                <div className="relative overflow-hidden px-6">
+                  <img
+                    src="https://github.com/AmineElazzab.png"
+                    className="max-w-full h-auto mx-auto rounded-full bg-gray-50 grayscale"
+                    alt="title image"
+                  />
+                </div>
+                <div className="pt-6 text-center">
+                  <p className="text-lg leading-normal font-bold mb-1">
+                    Mohammed Amine Elazzab
+                  </p>
+                  
+                  {/* social icon */}
+                  <div className="mt-2 mb-5 space-x-2">
+                    
+                    <a
+                      className="hover:text-blue-700"
+                      aria-label="Facebook link"
+                      href="#"
+                    >
+                      {/* <i class="fab fa-facebook text-facebook"></i> */}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="inline-block"
+                        width="1rem"
+                        height="1rem"
+                        viewBox="0 0 512 512"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M455.27,32H56.73A24.74,24.74,0,0,0,32,56.73V455.27A24.74,24.74,0,0,0,56.73,480H256V304H202.45V240H256V189c0-57.86,40.13-89.36,91.82-89.36,24.73,0,51.33,1.86,57.51,2.68v60.43H364.15c-28.12,0-33.48,13.3-33.48,32.9V240h67l-8.75,64H330.67V480h124.6A24.74,24.74,0,0,0,480,455.27V56.73A24.74,24.74,0,0,0,455.27,32Z"
+                        />
+                      </svg>
+                    </a>
+                    <a
+                      className="hover:text-blue-700"
+                      aria-label="Instagram link"
+                      href="#"
+                    >
+                      {/* <i class="fab fa-instagram text-instagram"></i> */}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="inline-block"
+                        width="1rem"
+                        height="1rem"
+                        viewBox="0 0 512 512"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M349.33,69.33a93.62,93.62,0,0,1,93.34,93.34V349.33a93.62,93.62,0,0,1-93.34,93.34H162.67a93.62,93.62,0,0,1-93.34-93.34V162.67a93.62,93.62,0,0,1,93.34-93.34H349.33m0-37.33H162.67C90.8,32,32,90.8,32,162.67V349.33C32,421.2,90.8,480,162.67,480H349.33C421.2,480,480,421.2,480,349.33V162.67C480,90.8,421.2,32,349.33,32Z"
+                        />
+                        <path
+                          fill="currentColor"
+                          d="M377.33,162.67a28,28,0,1,1,28-28A27.94,27.94,0,0,1,377.33,162.67Z"
+                        />
+                        <path
+                          fill="currentColor"
+                          d="M256,181.33A74.67,74.67,0,1,1,181.33,256,74.75,74.75,0,0,1,256,181.33M256,144A112,112,0,1,0,368,256,112,112,0,0,0,256,144Z"
+                        />
+                      </svg>
+                    </a>
+                    <a
+                      className="hover:text-blue-700"
+                      aria-label="Linkedin link"
+                      href="#"
+                    >
+                      {/* <i class="fab fa-linkedin text-linkedin"></i> */}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="inline-block"
+                        width="1rem"
+                        height="1rem"
+                        viewBox="0 0 512 512"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M444.17,32H70.28C49.85,32,32,46.7,32,66.89V441.61C32,461.91,49.85,480,70.28,480H444.06C464.6,480,480,461.79,480,441.61V66.89C480.12,46.7,464.6,32,444.17,32ZM170.87,405.43H106.69V205.88h64.18ZM141,175.54h-.46c-20.54,0-33.84-15.29-33.84-34.43,0-19.49,13.65-34.42,34.65-34.42s33.85,14.82,34.31,34.42C175.65,160.25,162.35,175.54,141,175.54ZM405.43,405.43H341.25V296.32c0-26.14-9.34-44-32.56-44-17.74,0-28.24,12-32.91,23.69-1.75,4.2-2.22,9.92-2.22,15.76V405.43H209.38V205.88h64.18v27.77c9.34-13.3,23.93-32.44,57.88-32.44,42.13,0,74,27.77,74,87.64Z"
+                        />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              {/* end team block */}
+            </div>
+          </div>
+          {/* end row */}
+        </div>
+      </div>
+
+      {/* <Footer /> */}
     </>
   );
 }
